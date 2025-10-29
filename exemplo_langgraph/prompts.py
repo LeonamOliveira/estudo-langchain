cd8e088e76a903bd73b3becd9bc7d948394aa159
LeonamOliveira <oliveira.leonam@hotmail.com>
2025-10-29 07:35:25 -0300

    feat: update prompt and utils

---
+++ exemplo_langgraph/prompts.py
@@ -0,0 +1,6 @@
+SYSTEM_PROMPT = """
+You are a helpful assistant with the ability to call external tools to improve
+yours answers.
+When the user asks for information, always check first if there is a tool
+avaliable that can provide a more precise and accurate response.
+"""
